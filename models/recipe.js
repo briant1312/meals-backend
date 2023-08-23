3e35bb4e9d9f82bf7fe64fa267c5829ce0b81a4e
Brian Triplett <briantriplett@Brians-MacBook-Air.local>
2023-08-23 19:16:40 -0400

    update fields on models

--- models/recipe.js
+++ models/recipe.js
@@ -1,15 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// do we want nutrition facts??
-// calories, carbs, fat, protein
-// nutrition label
-
-// flavor profile??
-// spice level??
-// 1 - 5
-
-
 const recipeSchema = new Schema({
     name: {
         type: String,
@@ -22,21 +13,19 @@ const recipeSchema = new Schema({
     origin: {
         type: String,
         // TODO: add enum for each region or create lookup table
-        enum: []
     },
-    // just need to know what ingredients are in the dish
     ingredients: [{
-        // name or ingredient??
-        _ingredient: {type: mongoose.Schema.Types.ObjectId, ref: "Ingredient"},
-        qty: {
-            type: Number,
-            required: true
-        },
-        unit: {
-            type: Number,
-            required: true
-        }
+        type: Schema.Types.ObjectId,
+        ref: "Ingredient"
     }],
+    flavorProfile: {
+        type: String
+    },
+    spiceLevel: {
+        type: Number,
+        min: 1,
+        max: 5
+    }
 })
 
 module.exports = mongoose.model("Recipe", recipeSchema);
